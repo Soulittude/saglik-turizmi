d0a15dd934daa0dfb503b8c287c70f093f70eeda
soulittude <79145776+Soulittude@users.noreply.github.com>
2025-07-17 23:58:19 +0300

    ServiceList created

---
+++ src/components/ServiceList.tsx
@@ -0,0 +1,17 @@
+import { Service } from "@/types";
+
+interface ServiceListProps {
+    services: Service[];
+}
+
+export default function ServiceList({ services }: ServiceListProps) {
+
+    return (<ul className="space-y-4">
+        {services.map((s, idx) => (
+            <li key={idx} className="flex justify-between border-b pb-2">
+                <span className="font-medium text-gray-800">{s.name}</span>
+                <span className="font-medium text-gray-600">{s.priceRange}</span>
+            </li>
+        ))}
+    </ul>);
+}
